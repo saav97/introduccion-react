1805f643d4c7199c6fc3e1d92a09b581417487bd
saav97 <sa.arriagada97@gmail.com>
2023-09-07 22:02:14 -0300

    firts commit

--- vite-project/src/homeApp.jsx
+++ vite-project/src/homeApp.jsx
@@ -1,21 +1,51 @@
-
-const nombre = {
-    nombre:"Alejadnro",
-    apellido:"Arriagada"
-};
-
-
-const getSuma = (a, b)=>{
-    return a+b;
-}
+import { useState } from 'react';
+import axios from 'axios';
 
 
 export const App = (props) => {
 
-    return (
-        <>
-            <h1>{props.title}</h1>
-            <p>{props.resp}</p>
-        </>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000/api/users',{username,password,email})
+    .then((resp)=>{
+      alert('Datos Cargados con exito!!');
+      console.log(resp);
+    })
+    .catch((error)=>{
+      alert('Anda pa ya Bobo!!');
+      console.log(error);
+    })
+  }
+
+  return (
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">username: <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} /> </label>
+
+        <br />
+        <br />
+        <br />
+        <label htmlFor="">password: <input type="password" name="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} /></label>
+
+        <br />
+        <br />
+        <br />
+        <label htmlFor="">Email: <input type="email" name="email" id="email" value={email} onChange={event => setEmail(event.target.value)} />
+        </label>
+
+        <br />
+        <br />
+        <button type='submit'>Enviar</button>
+
+
+      </form>
+    </>
+  );
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './homeApp';
 import './style.css';
+import {Lista} from './viewData';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App title="Hola soy tu padre" resp="nooooooo!"/>
+        <App title="Pepe"/>
+        <Lista></Lista>
     </React.StrictMode>
 )
